69777ee0197c3bbb0f5fd8f19ee9f805f0343b8b
2cu-1001 <137245969+2cu-1001@users.noreply.github.com>
2024-08-12 02:10:01 +0900

    Update main.py

--- samsung_black_box_opt/main.py
+++ samsung_black_box_opt/main.py
@@ -3,7 +3,6 @@ from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import PolynomialFeatures
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.decomposition import PCA
 import pandas as pd
 import os
@@ -17,56 +16,38 @@ def get_data():
     Xt = test_data.values[:, 1:]
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-
-    #return X_train, X_test, y_train, y_test, Xt
-
+    
     print("get_data() done")
-    return X, y, Xt
+    return X_train, X_test, y_train, y_test, Xt
 
-def preprocessing(X, Xt):
+
+def preprocessing(X_train, X_test, Xt):
     # stdsc = StandardScaler()
     # X_train_std = stdsc.fit_transform(X_train)
     # X_test_std = stdsc.transform(X_test)
     # Xt_std = stdsc.transform(Xt)
 
-    # pca = PCA(n_components=8)
-    # X_train_pca = pca.fit_transform(X_train)
-    # X_test_pca = pca.transform(X_test)
-    # Xt_pca = pca.transform(Xt)
-
     pca = PCA(n_components=8)
-    X_pca = pca.fit_transform(X)
+    X_train_pca = pca.fit_transform(X_train)
+    X_test_pca = pca.transform(X_test)
     Xt_pca = pca.transform(Xt)
-
-    # return X_train_std, X_test_std, Xt_std
-    # return X_train_pca, X_test_pca, Xt_pca
-
+    
     print("preprocessing() done")
-    return X_pca, Xt_pca
+    # return X_train_std, X_test_std, Xt_std
+    return X_train_pca, X_test_pca, Xt_pca
 
 
-def learning(X, y, Xt):
-    # poly = PolynomialFeatures(degree=8, include_bias=False)
-    # X_poly = poly.fit_transform(X_train)
-    # X_test_poly = poly.fit_transform(X_test)
-    # Xt_poly = poly.fit_transform(Xt)
-    #
-    # lr = Ridge(alpha = 0.01)
-    # lr.fit(X_poly, y_train)
-    # print('Training accuracy:', lr.score(X_poly, y_train))
-    # print('Test accuracy:', lr.score(X_test_poly, y_test))
-    # y_pred = lr.predict(Xt_poly)
+def learning(X_train, X_test, y_train, y_test, Xt):
+    poly = PolynomialFeatures(degree=8, include_bias=False)
+    X_poly = poly.fit_transform(X_train)
+    X_test_poly = poly.fit_transform(X_test)
+    Xt_poly = poly.fit_transform(Xt)
 
-    # lr = LinearRegression()
-    # lr.fit(X_train, y_train)
-    # print('Training accuracy:', lr.score(X_train, y_train))
-    # print('Test accuracy:', lr.score(X_test, y_test))
-    # y_pred = lr.predict(Xt)
-
-    lr = RandomForestRegressor()
-    lr.fit(X, y)
-    y_pred = lr.predict(Xt)
-    print('Training accuracy:', lr.score(X, y))
+    lr = Ridge(alpha=0.01)
+    lr.fit(X_poly, y_train)
+    print('Training accuracy:', lr.score(X_poly, y_train))
+    print('Test accuracy:', lr.score(X_test_poly, y_test))
+    y_pred = lr.predict(Xt_poly)
 
     print("learning() done")
     return y_pred
@@ -79,20 +60,18 @@ def make_submission_file(y_pred):
     submission.to_csv("real_submission.csv", index=False)
 
     print("make_submission_file() done")
+    
 
 def main():
     pre_file_path = "real_submission.csv"
     if os.path.isfile(pre_file_path):
         os.remove(pre_file_path)
 
-    # X_train, X_test, y_train, y_test, Xt = get_data()
-    X, y, Xt = get_data()
+    X_train, X_test, y_train, y_test, Xt = get_data()
+    
+    X_train_std, X_test_std, Xt_std = preprocessing(X_train, X_test, Xt)
 
-    #X_train X_test, Xt = preprocessing(X_train, X_test, Xt)
-    X, Xt = preprocessing(X, Xt)
-
-    #y_pred = learning(X_train, X_test, y_train, y_test, Xt)
-    y_pred = learning(X, y, Xt)
+    y_pred = learning(X_train_std, X_test_std, y_train, y_test, Xt_std)
 
     make_submission_file(y_pred)
 
