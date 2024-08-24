111b845b363fd10c879112c7f1e8de2b446544a3
2cu-1001 <137245969+2cu-1001@users.noreply.github.com>
2024-08-25 04:12:05 +0900

    Add files via upload

--- samsung_black_box_opt/accuracy_analyzer.py
+++ samsung_black_box_opt/accuracy_analyzer.py
@@ -11,36 +11,59 @@ from sklearn.preprocessing import StandardScaler
 import os
 from copy import deepcopy
 
-#0820, 0819, 0812
+#0822 0820, 0819, 0812
 
-df3rd = pd.read_csv("./real_submission_0820_1.csv")
-df2nd = pd.read_csv("./real_submission_0819_1.csv")
+df5th = pd.read_csv("./data/backup/0823/real_submission_0823.csv")
+df4th = pd.read_csv("./data/backup/0822/real_submission_0822.csv")
+df3rd = pd.read_csv("./data/backup/0820/real_submission_0820.csv")
+df2nd = pd.read_csv("./data/backup/0819/real_submission_0819.csv")
 df1st = pd.read_csv("./data/backup/0812/real_submission_0812.csv")
 
+df5th = df5th.sort_values(by=['y'], ascending=False)
+df4th = df4th.sort_values(by=['y'], ascending=False)
 df3rd = df3rd.sort_values(by=['y'], ascending=False)
 df2nd = df2nd.sort_values(by=['y'], ascending=False)
 df1st = df1st.sort_values(by=['y'], ascending=False)
 
+v5th = df5th['ID'].values[:500]
+v4th = df4th['ID'].values[:500]
 v3rd = df3rd['ID'].values[:500]
 v2nd = df2nd['ID'].values[:500]
 v1st = df1st['ID'].values[:500]
 
-tmp_correct_IDs = []
+tmp0_correct_IDs = []
 
-for t3rd in v3rd:
+for t4th in v4th:
+    for t5th in v5th:
+        if t5th == t4th:
+            tmp0_correct_IDs.append(t5th)
+print("tmp1 sz :", len(tmp0_correct_IDs))
+
+tmp1_correct_IDs = []
+
+for t4th in tmp0_correct_IDs:
+    for t3rd in v3rd:
+        if t3rd == t4th:
+            tmp1_correct_IDs.append(t3rd)
+print("tmp1 sz :", len(tmp1_correct_IDs))
+
+tmp2_correct_IDs = []
+
+for t3rd in tmp1_correct_IDs:
     for t2nd in v2nd:
         if t3rd == t2nd:
-            tmp_correct_IDs.append(t3rd)
+            tmp2_correct_IDs.append(t3rd)
+print("tmp2 sz :", len(tmp2_correct_IDs))
 
 final_correct_IDs = []
 
-for t2nd in tmp_correct_IDs:
+for t2nd in tmp2_correct_IDs:
     for t1st in v1st:
         if t2nd == t1st:
             final_correct_IDs.append(t2nd)
+print("final sz :", len(final_correct_IDs))
 
 print(final_correct_IDs)
-print(len(final_correct_IDs))
 
 cmp_path = "./real_submission.csv"
 df_cmp = pd.read_csv(cmp_path)
@@ -54,6 +77,17 @@ for t_cmp in v_cmp:
         if t_cmp == t_final:
             cmp_correct_IDs.append(t_cmp)
 
-
+cmp_correct_IDs = sorted(cmp_correct_IDs)
 print(cmp_correct_IDs)
 print(len(cmp_correct_IDs))
+
+
+t = []
+for t_cmp in v_cmp:
+    for t_final in v1st:
+        if t_cmp == t_final:
+            t.append(t_cmp)
+
+t = sorted(t)
+print(t)
+print(len(t))
--- samsung_black_box_opt/main.py
+++ samsung_black_box_opt/main.py
@@ -6,31 +6,83 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 import pandas as pd
+from sklearn.manifold import LocallyLinearEmbedding
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
+from umap import UMAP
 import os
 from copy import deepcopy
 
 
+class Model(nn.Module):
+    def __init__(self, dim):
+        super(Model, self).__init__()
+        self.layer1 = nn.Linear(dim, 3)
+        self.dropout1 = nn.Dropout(0.3)
+        # self.layer2 = nn.Linear(16, 8)
+        # self.dropout2 = nn.Dropout(0.3)
+        # self.layer3 = nn.Linear(16, 8)
+        # self.dropout3 = nn.Dropout(0.3)
+        self.output_layer = nn.Linear(3, 1)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.layer1(x))
+        x = self.dropout1(x)
+        # x = self.relu(self.layer2(x))
+        # x = self.dropout2(x)
+        # x = self.relu(self.layer3(x))
+        # x = self.dropout3(x)
+        return self.output_layer(x)
+
+
 def get_data():
+    # ------------------------------ load data ------------------------------#
     train_data = pd.read_csv("./data/train.csv")
     test_data = pd.read_csv("./data/test.csv")
     # train_data = train_data.drop(['x_0', 'x_1', 'x_2', 'x_3'], axis=1)
     # test_data = test_data.drop(['x_0', 'x_1', 'x_2', 'x_3'], axis=1)
+    # ------------------------------ load data ------------------------------#
 
+    # ------------------------------ remove outlier ------------------------------#
+    # sns.pairplot(train_data)
+    # plt.show()
+    # print(len(train_data))
+    # plt.scatter([i for i in range(len(train_data))], train_data['y'], s=2)
+    outlier = train_data[(abs((train_data['y'] - train_data['y'].mean()) / train_data['y'].std())) > 1.645].index
+    train_data = train_data.drop(outlier)
+    # plt.scatter([i for i in range(len(train_data))], train_data['y'], s=2, alpha=0.8)
+    # plt.show()
+
+    # sns.pairplot(train_data)
+    # plt.show()
     X = train_data.values[:, 1:-1]
     y = train_data.values[:, -1]
     Xt = test_data.values[:, 1:]
+    # ------------------------------ remove outlier ------------------------------#
 
-    scaler = StandardScaler()
-    X = scaler.fit_transform(X)
-    Xt = scaler.transform(Xt)
+    #------------------------------ dimension reduction ------------------------------#
+    # scaler = StandardScaler()
+    # X = scaler.fit_transform(X)
+    # Xt = scaler.transform(Xt)
 
-    pca = PCA(n_components=0.7)
-    X = pca.fit_transform(X)
-    dim = pca.n_components_
+    dim = 11
 
-    Xt = pca.transform(Xt)
+    # dim = 4
+    # lle = LocallyLinearEmbedding(n_components=dim)
+    # X = lle.fit_transform(X)
+    # Xt = lle.transform(Xt)
+
+    # pca = PCA(n_components=0.99)
+    # X = pca.fit_transform(X)
+    # Xt = pca.transform(Xt)
+    # dim = pca.n_components_
+
+    # dim = 8
+    # um = UMAP(n_components=dim, verbose=1)
+    # X = um.fit_transform(X)
+    # Xt = um.transform(Xt)
+
     print(X.shape)
     print(Xt.shape)
 
@@ -44,10 +96,8 @@ def get_data():
         ax.scatter(X[:, 0], X[:, 1], X[:, 2])
         ax.scatter(Xt[:, 0], Xt[:, 1], Xt[:, 2])
         plt.show()
+    # ------------------------------ dimension reduction ------------------------------#
 
-    # print(train_data)
-    # sns.pairplot(train_data)
-    # plt.show()
     X = torch.FloatTensor(X.astype("float64"))
     y = torch.FloatTensor(y.astype("float64")).unsqueeze(1)
     Xt = torch.FloatTensor(Xt.astype("float64"))
@@ -58,7 +108,8 @@ def get_data():
 
 def make_submission_file(y_pred):
     submission = pd.read_csv("./data/sample_submission.csv")
-    submission["y"] = y_pred.detach().cpu()
+    # submission["y"] = y_pred.detach().cpu()
+    submission["y"] = y_pred
 
     submission.to_csv("real_submission.csv", index=False)
 
@@ -66,6 +117,7 @@ def make_submission_file(y_pred):
 
 
 def main():
+    # ------------------------------ preprocessing ------------------------------#
     print(torch.cuda.is_available())
     print(torch.cuda.get_device_name(0))
     pre_file_path = "real_submission.csv"
@@ -75,23 +127,32 @@ def main():
     X, y, Xt, dim = get_data()
 
     # model = nn.Linear(11, 1)
-    model = nn.Linear(dim, 1)
+    # model = nn.Linear(dim, 1)
+    model = Model(dim)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
+    # ------------------------------ preprocessing ------------------------------#
 
+    # ------------------------------ model init ------------------------------#
     model = model.to(device)
+    nn.init.xavier_uniform_(model.layer1.weight)
+    # nn.init.xavier_uniform_(model.layer2.weight)
+    # nn.init.xavier_uniform_(model.layer3.weight)
     X, y, Xt, = X.to(device), y.to(device), Xt.to(device)
     print(next(model.parameters()).device)
 
     criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.0005, weight_decay=1e-4)
+    optimizer = optim.Adam(model.parameters(), lr=0.1, weight_decay=0.001)
     print("initial setting done")
 
     print(X.shape)
     print(Xt.shape)
+    # ------------------------------ model init ------------------------------#
 
-    for epoch in range(1, 250001):
+    # ------------------------------ learning ------------------------------#
+    for epoch in range(1, 1000001):
+        model.train()
         output = model(X)
         cost = criterion(output, y)
 
@@ -102,17 +163,20 @@ def main():
         if epoch % 100 == 0:
             print(f"Epoch : {epoch}, Model : {list(model.parameters())}, Cost : {cost}")
 
-        if cost < 1:
+        if cost < 5:
             break
+    # ------------------------------ learning ------------------------------#
 
+    # ------------------------------ predict ------------------------------#
     print(list(model.parameters()))
     y_pred = model(Xt)
 
+    X = X.cpu().numpy()
+    Xt = Xt.cpu().numpy()
+    y = y.cpu().numpy()
+    y_pred = y_pred.cpu().detach().numpy()
+
     if dim == 2:
-        X = X.cpu().numpy()
-        Xt = Xt.cpu().numpy()
-        y = y.cpu().numpy()
-        y_pred = y_pred.cpu().detach().numpy()
         fig = plt.figure()
         ax = fig.add_subplot(projection='3d')
         ax.scatter(X[:, 0], X[:, 1], y)
@@ -120,7 +184,7 @@ def main():
         plt.show()
 
     make_submission_file(y_pred)
-
+    # ------------------------------ predict ------------------------------#
 
 if __name__ == "__main__":
     main()
